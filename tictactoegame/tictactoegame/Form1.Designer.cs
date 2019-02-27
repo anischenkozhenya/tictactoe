5024fcc75f327681491855311b42375f36b20f97
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-02-27 21:02:34 +0300

    Add files via upload

---
+++ tictactoegame/tictactoegame/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace tictactoegame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button00 = new System.Windows.Forms.Button();
+            this.button01 = new System.Windows.Forms.Button();
+            this.button02 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.NGbutton = new System.Windows.Forms.Button();
+            this.Xwin = new System.Windows.Forms.Label();
+            this.Owin = new System.Windows.Forms.Label();
+            this.Drow = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button00
+            // 
+            this.button00.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button00.Location = new System.Drawing.Point(12, 12);
+            this.button00.Name = "button00";
+            this.button00.Size = new System.Drawing.Size(80, 80);
+            this.button00.TabIndex = 0;
+            this.button00.UseVisualStyleBackColor = true;
+            this.button00.Click += new System.EventHandler(this.button00_Click);
+            // 
+            // button01
+            // 
+            this.button01.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button01.Location = new System.Drawing.Point(98, 12);
+            this.button01.Name = "button01";
+            this.button01.Size = new System.Drawing.Size(80, 80);
+            this.button01.TabIndex = 1;
+            this.button01.UseVisualStyleBackColor = true;
+            this.button01.Click += new System.EventHandler(this.button01_Click);
+            // 
+            // button02
+            // 
+            this.button02.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button02.Location = new System.Drawing.Point(184, 12);
+            this.button02.Name = "button02";
+            this.button02.Size = new System.Drawing.Size(80, 80);
+            this.button02.TabIndex = 2;
+            this.button02.UseVisualStyleBackColor = true;
+            this.button02.Click += new System.EventHandler(this.button02_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(12, 98);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(80, 80);
+            this.button10.TabIndex = 3;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button11.Location = new System.Drawing.Point(98, 98);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(80, 80);
+            this.button11.TabIndex = 4;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button12.Location = new System.Drawing.Point(184, 98);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(80, 80);
+            this.button12.TabIndex = 5;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button20
+            // 
+            this.button20.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button20.Location = new System.Drawing.Point(12, 184);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(80, 80);
+            this.button20.TabIndex = 6;
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button21.Location = new System.Drawing.Point(98, 184);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(80, 80);
+            this.button21.TabIndex = 7;
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button22.Location = new System.Drawing.Point(184, 184);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(80, 80);
+            this.button22.TabIndex = 8;
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // NGbutton
+            // 
+            this.NGbutton.Location = new System.Drawing.Point(17, 311);
+            this.NGbutton.Name = "NGbutton";
+            this.NGbutton.Size = new System.Drawing.Size(75, 23);
+            this.NGbutton.TabIndex = 9;
+            this.NGbutton.Text = "New game";
+            this.NGbutton.UseVisualStyleBackColor = true;
+            this.NGbutton.Click += new System.EventHandler(this.NGbutton_Click);
+            // 
+            // Xwin
+            // 
+            this.Xwin.AutoSize = true;
+            this.Xwin.Location = new System.Drawing.Point(344, 99);
+            this.Xwin.Name = "Xwin";
+            this.Xwin.Size = new System.Drawing.Size(17, 13);
+            this.Xwin.TabIndex = 13;
+            this.Xwin.Text = "X:";
+            // 
+            // Owin
+            // 
+            this.Owin.AutoSize = true;
+            this.Owin.Location = new System.Drawing.Point(344, 132);
+            this.Owin.Name = "Owin";
+            this.Owin.Size = new System.Drawing.Size(18, 13);
+            this.Owin.TabIndex = 14;
+            this.Owin.Text = "O:";
+            // 
+            // Drow
+            // 
+            this.Drow.AutoSize = true;
+            this.Drow.Location = new System.Drawing.Point(344, 168);
+            this.Drow.Name = "Drow";
+            this.Drow.Size = new System.Drawing.Size(32, 13);
+            this.Drow.TabIndex = 15;
+            this.Drow.Text = "Draw";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Drow);
+            this.Controls.Add(this.Owin);
+            this.Controls.Add(this.Xwin);
+            this.Controls.Add(this.NGbutton);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button02);
+            this.Controls.Add(this.button01);
+            this.Controls.Add(this.button00);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "TIC TAC TOE GAME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button00;
+        private System.Windows.Forms.Button button01;
+        private System.Windows.Forms.Button button02;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button NGbutton;
+        private System.Windows.Forms.Label Xwin;
+        private System.Windows.Forms.Label Owin;
+        private System.Windows.Forms.Label Drow;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ tictactoegame/tictactoegame/Form1.cs
@@ -0,0 +1,484 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tictactoegame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        public int player = 2;
+        public int turns = 0;
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0;
+        public void NewGame()
+        {
+            button00.Text = ""; button01.Text = ""; button02.Text = "";
+            button10.Text = ""; button11.Text = ""; button12.Text = "";
+            button20.Text = ""; button21.Text = ""; button22.Text = "";
+            button00.Enabled = button01.Enabled = button02.Enabled = button10.Enabled = button11.Enabled
+            = button12.Enabled = button20.Enabled = button21.Enabled = button22.Enabled = true;
+            turns = 0;
+            
+
+        }
+                
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Xwin.Text = "X: " + s1;
+            Owin.Text = "O: " + s2;
+            Drow.Text = "Drow: " + sd;
+        }
+
+        private void NGbutton_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+        bool CheckDraw()
+        {
+            if (turns == 9)
+            {
+
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        bool CheckWinner()
+        {
+            if ((button00.Text == button01.Text) && (button01.Text == button02.Text) && (button00.Text != ""))
+            {
+                return true;
+            }
+            else if ((button10.Text == button11.Text) && (button11.Text == button12.Text) && (button10.Text != ""))
+            {
+                return true;
+            }
+            else if ((button20.Text == button21.Text) && (button21.Text == button22.Text) && (button20.Text != ""))
+            {
+                return true;
+            }
+            else if ((button00.Text == button10.Text) && (button10.Text == button20.Text) && (button00.Text != ""))
+            {
+                return true;
+            }
+            else if ((button01.Text == button11.Text) && (button11.Text == button21.Text) && (button01.Text != ""))
+            {
+                return true;
+            }
+            else if ((button02.Text == button12.Text) && (button12.Text == button22.Text) && (button02.Text != ""))
+            {
+                return true;
+            }
+            else if ((button00.Text == button11.Text) && (button11.Text == button22.Text) && (button00.Text != ""))
+            {
+                return true;
+            }
+            else if ((button02.Text == button11.Text) && (button11.Text == button20.Text) && (button02.Text != ""))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void button00_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+
+
+        }
+
+        private void button01_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+
+        }
+
+        private void button02_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (player % 2 == 0)
+            {
+                button.Text = "X";
+                player++;
+                turns++;
+            }
+            else
+            {
+                button.Text = "0";
+                player++;
+                turns++;
+            }
+            button.Enabled = false;
+            if (CheckDraw() == true)
+            {
+                MessageBox.Show("Tie game!");
+                sd++;
+                NewGame();
+            }
+            if (CheckWinner() == true)
+            {
+                if (button.Text == "X")
+                {
+                    MessageBox.Show("X Won!");
+                    s1++;
+                    NewGame();
+                }
+                else
+                {
+                    MessageBox.Show(") Won!");
+                    s2++;
+                    NewGame();
+                }
+            }
+            Form1_Load(null, null);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s1 = 0;
+            s2 = 0;
+            sd = 0;
+            turns = 0;
+            Form1_Load(null, null);
+        }
+    }
+}
